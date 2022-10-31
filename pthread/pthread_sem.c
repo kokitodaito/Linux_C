7dbdaa631eade1072a85d3fdee29d6bbcfb2301c
kokitodaito <342800903@qq.com>
2022-10-31 09:55:35 +0800

    sem 2

--- pthread/pthread_sem.c
+++ pthread/pthread_sem.c
@@ -33,5 +33,47 @@ void *thrd_func(void *arg)
 int main(void)
 {
     pthread_t thread[THREAD_NUMBER];
-    
+    int no = 0,res;
+    void *thrd_ret;
+
+    srand(time(NULL));
+    for (no = 0;no < THREAD_NUMBER;no++)
+    {
+        /*信号量初始化为0*/
+        sem_init(&sem[no],0,0);
+        res = pthread_create(&thread[no], NULL, thrd_func, (void*)no);
+        if (res != 0)
+        {
+            printf("Create thread %d failed\n", no);
+            exit(res);
+        }
+    }
+
+    printf("Create threads success\n Waiting for threads to finish\n");
+
+    /*对第一个创建的线程执行V操作*/
+    sem_post(&sem[0]);
+    for (no = 0;no < THREAD_NUMBER-1;no++)
+    {
+        res = pthread_join(thread[no], &thrd_ret);
+        if (!res)
+        {
+            printf("Thread %d joined\n",no);
+        }
+        else
+        {
+            printf("Thread %d join failed\n",no);
+        }
+        /*V操作*/
+        sem_post(&sem[no+1]);
+    }
+
+    pthread_join(thread[2],&thrd_ret);
+
+    for (no=0;no<THREAD_NUMBER;no++)
+    {
+        sem_destroy(&sem[no]);
+    }
+
+    return 0;
 }
